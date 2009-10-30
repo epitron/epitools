5df27d311b0682ca983882d3b8208b6c6ac0b106
epitron <chris@ill-logic.com>
2009-10-30 17:47:25 -0400

    Damn you, tabs and DOS endings!

--- lib/epitools/http.rb
+++ lib/epitools/http.rb
@@ -13,27 +13,25 @@ def read_data_from_response(response, amount)
   chunks = []
   
   begin
+    response.read_body do |chunk|   # read body now
       
-      response.read_body do |chunk|   # read body now
-        
-        amount_read += chunk.length
-        
-        if amount_read > amount
-          amount_of_overflow = amount_read - amount
-          chunk = chunk[0...-amount_of_overflow]
-        end
-        
-        chunks << chunk
-  
-        if amount_read >= amount
-            raise PartialPage.new chunks.join('')
-        end
-        
+      amount_read += chunk.length
+      
+      if amount_read > amount
+        amount_of_overflow = amount_read - amount
+        chunk = chunk[0...-amount_of_overflow]
       end
+      
+      chunks << chunk
+
+      if amount_read >= amount
+          raise PartialPage.new chunks.join('')
+      end
+      
+    end
   end
   
 end
-
 
 
 def http_get_streaming(url = URI.parse("http://epi.is-a-geek.net/files/Mr.%20Show%20-%20Civil%20War%20Re-enactment.avi"))
@@ -42,22 +40,22 @@ def http_get_streaming(url = URI.parse("http://epi.is-a-geek.net/files/Mr.%20Sho
   headers = nil
   
   Net::HTTP.start(url.host, url.port) do |http|
-      # using block
-      response = http.request_get(url.path, headers) {|response|
-        puts "Response: #{response.inspect}"
-        puts "to hash: #{response.to_hash.inspect}"
-  
-        begin
-          read_data_from_response(response, 500)
-        rescue PartialPage => p
-          puts "GOT THE PARTIAL PAGE!"
-          data = p.data
-        end
-      
-        puts
-        puts "===========first 500 bytes================="
-        puts data
-      }    
+    # using block
+    response = http.request_get(url.path, headers) {|response|
+      puts "Response: #{response.inspect}"
+      puts "to hash: #{response.to_hash.inspect}"
+
+      begin
+        read_data_from_response(response, 500)
+      rescue PartialPage => p
+        puts "GOT THE PARTIAL PAGE!"
+        data = p.data
+      end
+    
+      puts
+      puts "===========first 500 bytes================="
+      puts data
+    }    
   end
 
 end
@@ -69,16 +67,16 @@ def http_get_cached(url)
   require 'digest/md5'
   require 'rio'
   
-	tempdir = ENV['TEMP']
-	cachefile = rio(tempdir, "cached_url_#{Digest::SHA1.hexdigest(url)}")
-	
-	if cachefile.exist?
-		data = rio(cachefile).read
-	else
-		data = rio(url).read
-		rio(cachefile).binmode < data
-	end
-	
-	data
+  tempdir = ENV['TEMP']
+  cachefile = rio(tempdir, "cached_url_#{Digest::SHA1.hexdigest(url)}")
+  
+  if cachefile.exist?
+    data = rio(cachefile).read
+  else
+    data = rio(url).read
+    rio(cachefile).binmode < data
+  end
+  
+  data
 end
 
