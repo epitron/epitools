cee89c1d9a468faf58128fbe1698ebd1c93396ba
epitron <chris@ill-logic.com>
2011-01-30 05:49:02 -0500

    Moved __DIR__ from basetypes to the main epitools.rb file.

--- lib/epitools.rb
+++ lib/epitools.rb
@@ -1,4 +1,18 @@
-__DIR__ = File.dirname(__FILE__)
+class Object
+
+  unless defined?(__DIR__)
+    # 
+    # This method is convenience for the `File.expand_path(File.dirname(__FILE__))` idiom.
+    # (taken from Michael Fellinger's Ramaze... thanx, dood! :D)
+    #
+    def __DIR__(*args)
+      filename = caller[0][/^(.*):/, 1]
+      dir = File.expand_path(File.dirname(filename))
+      ::File.expand_path(::File.join(dir, *args.map{|a| a.to_s}))
+    end
+  end
+  
+end
 
 require_wrapper = proc do |mod|
   begin
