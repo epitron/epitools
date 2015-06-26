eb62bd2b078882af1a1256d97ac790a7d1b701d9
Chris Gahan <chris@ill-logic.com>
2015-06-26 11:24:07 -0400

    'Kernel.daemonize' unhooks your pipes and forks you into the background

---
+++ lib/epitools/daemonize.rb
@@ -0,0 +1,38 @@
+module Kernel
+  # This method causes the current running process to become a daemon
+  # All further printing is relied to the error.log
+  # FIXME doesn't belong into Butler::Bot, rather into botcontrol
+  def daemonize(chdir=nil, &on_sighup)
+    srand # Split rand streams between spawning and daemonized process
+    safe_fork and exit # Fork and exit from the parent
+
+    # Detach from the controlling terminal
+    raise "Can't detach from controlling terminal" unless sess_id = Process.setsid
+
+    # Prevent the possibility of acquiring a controlling terminal
+    trap('SIGHUP', 'IGNORE')
+    exit if safe_fork
+
+    # In daemon mode, a SIGHUP means termination
+    trap('SIGHUP', &on_sighup)
+
+    # We can't get the originally controlling terminals stdout/stdin anymore
+    STDIN.reopen("/dev/null")
+    STDOUT.reopen("/dev/null", "a")
+    STDERR.reopen(STDOUT)
+    
+    Dir.chdir(chdir) if chdir
+    File.umask 0033 # FIXME ask somebody knowledgable about a sensible value
+
+    sess_id
+  end
+
+  # Try to fork if at all possible retrying every +delay+ sec (5s default)
+  # if the maximum process limit for the system has been reached
+  def safe_fork(delay=5)
+    fork
+  rescue Errno::EWOULDBLOCK
+    sleep(delay)
+    retry
+  end
+end
