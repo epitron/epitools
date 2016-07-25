0fd14269e7c2057b379f5d455a878158f89ecb9f
epitron <chris@ill-logic.com>
2016-07-25 16:11:30 -0400

    Autoload more digest/sha2 modules

--- lib/epitools/autoloads.rb
+++ lib/epitools/autoloads.rb
@@ -24,6 +24,9 @@ autoload :SDBM,       'sdbm'
 module Digest
   autoload :SHA1,     'digest/sha1'
   autoload :SHA2,     'digest/sha2'
+  autoload :SHA256,   'digest/sha2'
+  autoload :SHA384,   'digest/sha2'
+  autoload :SHA512,   'digest/sha2'
   autoload :MD5,      'digest/md5'
 end
 
