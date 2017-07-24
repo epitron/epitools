bdd3a3785aefde160ee91afb6712f24bb7bbf14e
epitron <chris@ill-logic.com>
2017-07-24 01:22:49 -0400

    'String#words' now exists, and has a list of STOP_WORDS

--- lib/epitools/core_ext/string.rb
+++ lib/epitools/core_ext/string.rb
@@ -5,6 +5,16 @@ require 'epitools/core_ext/numbers'
 class String
 
   #
+  # For `titlecase`
+  #
+  LOWERCASE_WORDS = Set.new %w[of to or the and an a at is for from in]
+
+  #
+  # For `words_without_stopwords`
+  #
+  STOP_WORDS = %w[a cannot into our thus about co is ours to above could it ourselves together across down its out too after during itself over toward afterwards each last own towards again eg latter per under against either latterly perhaps until all else least rather up almost elsewhere less same upon alone enough ltd seem us along etc many seemed very already even may seeming via also ever me seems was although every meanwhile several we always everyone might she well among everything more should were amongst everywhere moreover since what an except most so whatever and few mostly some when another first much somehow whence any for must someone whenever anyhow former my something where anyone formerly myself sometime whereafter anything from namely sometimes whereas anywhere further neither somewhere whereby are had never still wherein around has nevertheless such whereupon as have next than wherever at he no that whether be hence nobody the whither became her none their which because here noone them while become hereafter nor themselves who becomes hereby not then whoever becoming herein nothing thence whole been hereupon now there whom before hers nowhere thereafter whose beforehand herself of thereby why behind him off therefore will being himself often therein with below his on thereupon within beside how once these without besides however one they would between i only this yet beyond ie onto those you both if or though your but in other through yours by inc others throughout yourself can indeed otherwise thru yourselves]
+
+  #
   # Convert \r\n to \n
   #
   def to_unix
@@ -24,11 +34,6 @@ class String
   def dewhitespace
     gsub(/\s+/,' ').strip
   end
-
-  #
-  # For `titlecase`
-  #
-  LOWERCASE_WORDS = Set.new %w[of to or the and an a at is for from in]
 
   #
   # Return a new string converted to "Title Case" (first letter of each word capitalized)
@@ -129,13 +134,12 @@ class String
   #
   def wrap(width=nil)
     if width.nil? or width < 0
-      require 'io/console'
-      _, winwidth = STDIN.winsize
+      term_width, _ = Term.size
 
-      if width < 0
-        width = (winwidth - 1) + width
+      if width and width < 0
+        width = (term_width - 1) + width
       else
-        width = winwidth - 1
+        width = term_width - 1
       end
     end
 
@@ -187,6 +191,16 @@ class String
     wrap(width).each_line.map { |line| prefix + line }.join
   end
   alias_method :wrapdent, :wrap_and_indent
+
+  def words
+    scan /\w+/
+    # scan /^[a-z]+$|^\w+\-\w+|^[a-z]+[0-9]+[a-z]+$|^[0-9]+[a-z]+|^[a-z]+[0-9]+$/
+  end
+
+  def words_without_stopwords
+    downcase.words - STOP_WORDS
+  end
+  alias_method :without_stopwords, :words_without_stopwords
 
   #
   # Iterate over slices of the string of size `slice_width`.
