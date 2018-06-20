24c9e8af5072bdb9ba97be2326305bab63bbaa95
epitron <chris@ill-logic.com>
2018-06-19 20:17:24 -0400

    'Object.memoize' improved, and moved to 'Module#memoize'

--- lib/epitools/core_ext/object.rb
+++ lib/epitools/core_ext/object.rb
@@ -75,27 +75,6 @@ class Object
   end
 
   #
-  # Cache (memoize) the result of an instance method the first time
-  # it's called, storing this value in the "@methodname" instance variable,
-  # and always return this value on subsequent calls.
-  #
-  def self.memoize(*methods)
-    methods.each do |meth|
-      old_method = instance_method(meth)
-
-      if old_method.arity == 0
-        ivarname = "@#{meth}"
-
-        define_method meth do
-          instance_variable_get(ivarname) or instance_variable_set(ivarname, old_method.bind(self).call)
-        end
-      else
-        raise "Can't memoize methods with arguments. (YET.)"
-      end
-    end
-  end
-
-  #
   # Serialize this object to a binary String, using Marshal.dump.
   #
   def marshal
@@ -231,3 +210,26 @@ class NotWrapper < BasicObject # :nodoc:
   end
 end
 
+
+class Module
+
+  #
+  # Cache (memoize) the result of an instance method the first time
+  # it's called, storing this value in the "@__memoized_#{methodname}_cache"
+  # instance variable, and always return this value on subsequent calls
+  # (unless the returned value is nil).
+  #
+  def memoize(*methods)
+    # alias_method is faster than define_method + old.bind(self).call
+    methods.each do |meth|
+      alias_method "__memoized__#{meth}", meth
+      module_eval <<-EOF
+        def #{meth}(*a, &b)
+          # assumes the block won't change the result if the args are the same
+          (@__memoized_#{meth}_cache ||= {})[a] ||= __memoized__#{meth}(*a, &b)
+        end
+      EOF
+    end
+  end
+
+end  
