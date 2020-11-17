7042a2aa118ef17cac8cfedbfc384f0706ff98ab
epitron <chris@ill-logic.com>
2020-11-16 23:23:04 -0500

    'Class#using' removed, 'Class#trace_messages_to' fixed

--- lib/epitools/core_ext/class.rb
+++ lib/epitools/core_ext/class.rb
@@ -1,31 +1,17 @@
 class Class
 
   #
-  # Return a copy of the class with modules mixed into it.
-  #
-  def self.using(*args)
-    if block_given?
-      yield using(*args)
-    else
-      copy = self.dup
-      args.each { |arg| copy.send(:include, arg) }
-      copy
-    end
-  end
-
-
-  #
   # Trace the specified method calls (`meths`, as symbols) to descendends of this class (or all methods if `:*` is supplied).
   # Output is printed to $stderr.
   #
   def trace_messages_to(*meths)
     return unless $DEBUG
-    
+
     tracers = Module.new
     parent  = self
 
     $stderr.puts "[*] Tracing messages sent to #{parent} (messages: #{meths.join(", ")})"
-    
+
     meths.each do |meth|
       case meth
       when :*
@@ -35,9 +21,9 @@ class Class
         end
       else
         tracers.define_method(meth) do |*args, &block|
-          args = args.map(&:inspect)
-          args << "&block" if block
-          $stderr.puts "[*] #{parent}##{meth}(#{args.join(", ")})"
+          arg_names = args.map(&:inspect)
+          arg_names << "&block" if block
+          $stderr.puts "[*] #{parent}##{meth}(#{arg_names.join(", ")})"
           if block
             super(*args, &block)
           else
